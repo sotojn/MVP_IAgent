dc35ba9b26a916d9e236f50f3adb44fc3d01e8ea
Joseph <josephsoto66@gmail.com>
2023-01-12 09:01:58 -0700

    Setup dev environment and package

---
+++ client/src/components/Header.js
@@ -0,0 +1,18 @@
+import '../App.css';
+
+function Header() {
+
+
+  return (
+    <section className="row">
+      <div className="grid">
+        <div className="col-1-3">
+
+          <h1>I Agent</h1>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Header;
